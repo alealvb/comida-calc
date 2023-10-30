91c263e95572ddf800ff587c9d528f1a85d3348a
Alejandro Alvarez <alejandro.alvarez.coder@gmail.com>
2023-10-29 22:29:28 -0300

    feat(db): :card_file_box: basic database schema

--- src/app/_components/create-product.tsx
+++ src/app/_components/create-product.tsx
@@ -5,11 +5,11 @@ import { useState } from "react";
 
 import { api } from "~/trpc/react";
 
-export function CreatePost() {
+export function CreateProduct() {
   const router = useRouter();
   const [name, setName] = useState("");
 
-  const createPost = api.post.create.useMutation({
+  const createProduct = api.product.create.useMutation({
     onSuccess: () => {
       router.refresh();
       setName("");
@@ -20,13 +20,13 @@ export function CreatePost() {
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        createPost.mutate({ name });
+        createProduct.mutate({ name });
       }}
       className="flex flex-col gap-2"
     >
       <input
         type="text"
-        placeholder="Title"
+        placeholder="Name"
         value={name}
         onChange={(e) => setName(e.target.value)}
         className="w-full rounded-full px-4 py-2 text-black"
@@ -34,9 +34,9 @@ export function CreatePost() {
       <button
         type="submit"
         className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-        disabled={createPost.isLoading}
+        disabled={createProduct.isLoading}
       >
-        {createPost.isLoading ? "Submitting..." : "Submit"}
+        {createProduct.isLoading ? "Submitting..." : "Submit"}
       </button>
     </form>
   );
--- src/server/api/routers/product.ts
+++ src/server/api/routers/product.ts
@@ -6,7 +6,7 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 
-export const postRouter = createTRPCRouter({
+export const productRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
@@ -18,10 +18,7 @@ export const postRouter = createTRPCRouter({
   create: protectedProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      return ctx.db.post.create({
+      return await ctx.db.product.create({
         data: {
           name: input.name,
           createdBy: { connect: { id: ctx.session.user.id } },
@@ -30,7 +27,7 @@ export const postRouter = createTRPCRouter({
     }),
 
   getLatest: protectedProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
+    return ctx.db.product.findFirst({
       orderBy: { createdAt: "desc" },
       where: { createdBy: { id: ctx.session.user.id } },
     });
